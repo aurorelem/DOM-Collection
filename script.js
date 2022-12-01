d0c4a1ca9a0b8efe894bd40f23c0abc44430abd1
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-01 15:25:37 +0100

    dernières modifications

--- script.js
+++ script.js
@@ -1,1 +1,206 @@
+const collection = [
+    {
+      title: "In the shadow of the moon",
+      kind: "Movie",
+      genre: "Thriller",
+      realisator: "Jim Mickle",
+      releaseYear: "2020",
+      cast: [
+        "Michael C. Hall",
+        "Boyd Holbrook",
+        "Cleopatra Coleman",
+      ],
+      description: "Un policier est obsédé par la traque d'un mystérieux tueur en série dont les crimes défient toutes explications...",
+      picture: "assets/In the shadow of the mood",  // Vérifier que ça fct bien
+      link: "https://www.youtube.com/watch?v=iU7B59T1l70",
+    },
+  ];
 
+  [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
+
+  const collection = [
+    {
+      title: "",
+      kind: "",
+      genre: ["", "",""],
+      realisator: "",
+      releaseYear: "",
+      cast: [
+        "",
+        "",
+        "",
+        "",
+      ],
+      description: "",
+      picture: "link/to/a/picture",
+      link: "",
+    },
+  
+    // ...
+  ];
