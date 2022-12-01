b82ed2f9b975877318a5b43d687efc0d8a7cae73
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-01 16:48:25 +0100

    fin de journée

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const collection = [
     {
       title: "Une offrande à la tempête",
       kind: "Movie",
-      genre: ["", "",""],
+      genre: ["Policier", "Thriller"],
       realisator: "Fernando González Molina",
       releaseYear: "2021",
       cast: [
@@ -48,26 +48,102 @@ const collection = [
     },
 
     {
-      title: "",
-      kind: "",
-      genre: ["", "",""],
-      realisator: "",
-      releaseYear: "",
+      title: "L'aliéniste",
+      kind: "Serie",
+      genre: ["Drame", "Historique","Policier"],
+      realisator: "Hossein Amini",
+      releaseYear: "2018",
       cast: [
-        "",
-        "",
-        "",
-        "",
+        "Daniel Brühl",
+        "Luke Evans",
+        "Dakota Fanning",
       ],
-      description: "",
-      picture: "link/to/a/picture",
-      link: "",
+      description: "Dans le New York de la fin du 19ème siècle sous la responsabilité du chef de police Theodore Roosevelt, le spécialiste des maladies mentales Laszlo Kreizler s'intéresse à un impitoyable tueur qui laisse derrière lui les corps mutilés d’enfants. Face à l'impassibilité des pouvoirs publics, Kreizler tente d’identifier le responsable de ces meurtres horribles, épaulé par l'illustrateur criminel John Moore et Sara Howard, la première femme à faire son entrée dans l'univers très masculin de la police.",
+      picture: "asset/l-aliéniste.jpg",
+      link: "\watch",
     },
   
-    // ...
-  ];
+    {
+      title: "Mindhunter",
+      kind: "Serie",
+      genre: ["Drame", "Policier","Thriller"],
+      realisator: "Joe Penhall",
+      releaseYear: "2017",
+      cast: [
+        "Jonathan Groff II",
+        "Holt McCallany",
+        "Anna Torv",
+      ],
+      description: "Comment anticiper la folie quand on ignore comment fonctionnent les fous ? Deux agents du FBI imaginent une enquête aux méthodes révolutionnaires et se lancent dans une véritable odyssée pour obtenir des réponses.",
+      picture: "assets/mindhunter.jpg",
+      link: "\watch",
+    },
+  
+    {
+      title: "Dahmer",
+      kind: "Serie",
+      genre: ["Drame", "Thriller","Biopic"],
+      realisator: "Ryan Murphy",
+      releaseYear: "2022",
+      cast: [
+        "Evan Peters",
+        "Richard Jenkins",
+        "Penelope Ann Miller",
+      ],
+      description: "Le parcours du cannibale de Milwaukee, l’un des tueurs en série les plus connus aux Etats-Unis : de son enfance difficile à sa condamnation en 1992, et comment l’incompétence et l’apathie de la police lui ont permis de poursuivre ses crimes durant plusieurs années.",
+      picture: "assets/dahmer.jpg",
+      link: "\watch",
+    },
+  
+    {
+      title: "Zodiac",
+      kind: "Serie",
+      genre: "Thriller",
+      realisator: "David Fincher",
+      releaseYear: "2007",
+      cast: [
+        "Jake Gyllenhaal",
+        "Mark Ruffalo",
+        "Anthony Edwards",
+      ],
+      description: `Zodiac, l'insaisissable tueur en série qui sévit à la fin des années 60 et répandit la terreur dans la région de San Francisco, fut le Jack l'Eventreur de l'Amérique. Prodigue en messages cryptés, il semait les indices comme autant de cailloux blancs, et prenait un malin plaisir à narguer la presse et la police. Il s'attribua une trentaine d'assassinats, mais fit bien d'autres dégâts collatéraux parmi ceux qui le traquèrent en vain.
+      Robert Graysmith, jeune et timide dessinateur de presse, n'avait ni l'expérience ni les relations de son brillant collègue Paul Avery, spécialiste des affaires criminelles au San Francisco Chronicle. Extérieur à l'enquête, il n'avait pas accès aux données et témoignages dont disposait le charismatique Inspecteur David Toschi et son méticuleux partenaire, l'Inspecteur William Armstrong. Le Zodiac n'en deviendrait pas moins l'affaire de sa vie, à laquelle il consacrerait dix ans d'efforts et deux ouvrages d'une vertigineuse précision..`,
+      picture: "asset/zodiac.jpg",
+      link: "https://www.youtube.com/watch?v=6bFZL61OFzU",
+    },
+  
+    {
+      title: "Octobre",
+      kind: "Movie",
+      genre: ["Policier", "Thriller"],
+      realisator: "Soren Sveistrup",
+      releaseYear: "2021",
+      cast: [
+        "Esben Dalgaard Andersen",
+        "Danica Curcic",
+        "David Dencik",
+      ],
+      description: "Deux détectives de Copenhague enquêtent sur une affaire de meurtre ultra médiatisée.",
+      picture: "assets/octobre.jpg",
+      link: "\watch",
+    },
+  
+    {
+      title: "De chair et d'os",
+      kind: "Movie",
+      genre: ["Policier", "Thriller"],
+      realisator: "Fernando González Molina",
+      releaseYear: "2020",
+      cast: [
+        "Marta Etura",
+        "Nene",
+        "Leonardo Sbaraglia",
+      ],
+      description: "Un an après avoir résolu une série de meurtres, Amaia est touchée de près par un mystère lié à sa précédente enquête. Deuxième partie de la trilogie du Baztan.",
+      picture: "assets/de-chair-et-d-os.jpg",
+      link: "\watch",
+    },
 
-  const collection = [
     {
       title: "",
       kind: "",
@@ -85,110 +161,5 @@ const collection = [
       link: "",
     },
   
-    // ...
-  ];
 
-  const collection = [
-    {
-      title: "",
-      kind: "",
-      genre: ["", "",""],
-      realisator: "",
-      releaseYear: "",
-      cast: [
-        "",
-        "",
-        "",
-        "",
-      ],
-      description: "",
-      picture: "link/to/a/picture",
-      link: "",
-    },
-  
-    // ...
-  ];
-
-  const collection = [
-    {
-      title: "",
-      kind: "",
-      genre: ["", "",""],
-      realisator: "",
-      releaseYear: "",
-      cast: [
-        "",
-        "",
-        "",
-        "",
-      ],
-      description: "",
-      picture: "link/to/a/picture",
-      link: "",
-    },
-  
-    // ...
-  ];
-
-  const collection = [
-    {
-      title: "",
-      kind: "",
-      genre: ["", "",""],
-      realisator: "",
-      releaseYear: "",
-      cast: [
-        "",
-        "",
-        "",
-        "",
-      ],
-      description: "",
-      picture: "link/to/a/picture",
-      link: "",
-    },
-  
-    // ...
-  ];
-
-  const collection = [
-    {
-      title: "",
-      kind: "",
-      genre: ["", "",""],
-      realisator: "",
-      releaseYear: "",
-      cast: [
-        "",
-        "",
-        "",
-        "",
-      ],
-      description: "",
-      picture: "link/to/a/picture",
-      link: "",
-    },
-  
-    // ...
-  ];
-
-  const collection = [
-    {
-      title: "",
-      kind: "",
-      genre: ["", "",""],
-      realisator: "",
-      releaseYear: "",
-      cast: [
-        "",
-        "",
-        "",
-        "",
-      ],
-      description: "",
-      picture: "link/to/a/picture",
-      link: "",
-    },
-  
-    // ...
   ];
